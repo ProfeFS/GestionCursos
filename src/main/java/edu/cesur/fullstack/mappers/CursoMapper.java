a34d59e4f5fbe984b7e621a5357e95034a10ad73
ProfeFS <profefs@outlook.com>
2023-11-22 12:03:31 +0100

    no message

--- src/main/java/edu/cesur/fullstack/mappers/CursoMapper.java
+++ src/main/java/edu/cesur/fullstack/mappers/CursoMapper.java
@@ -6,7 +6,6 @@ import org.mapstruct.Mapping;
 import edu.cesur.fullstack.dtos.CursoDTO;
 import edu.cesur.fullstack.persistence.entities.CursoEntity;
 
-// Importaciones necesarias
 @Mapper(componentModel = "spring", uses = {EstudianteMapper.class})
 public interface CursoMapper {
     CursoDTO toDto(CursoEntity curso);
